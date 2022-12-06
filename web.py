d182147b11001ddc682555c8082323678c9b0808
mark172mark172 <mark172mark172@gmail.com>
2022-12-06 21:39:02 +0800

    Add files via upload

---
+++ web.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "hellp world"
+
+@app.route("/test")
+def test():
+    return "test page"
+
+if __name__ == "__main__":
+    app.run()
